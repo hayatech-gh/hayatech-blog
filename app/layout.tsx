ddbb8ec2965f4e239c21be4da5d5d320b1875045
H４Y４T３ <hayate@HayatenoMacBook-Pro.local>
2025-03-22 09:44:56 +0900

    Fix OG 250322

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,22 +6,30 @@ import type { Metadata } from 'next';
 const hayaTechBlog = 'HayaTech-Blog';
 
 export const metadata: Metadata = {
-  title: 'HayaTech-Blog',
+  title: hayaTechBlog,
   description:
-    'HayaTech-Blogでは、エンジニアのための情報共有コミュニティであるに投稿している記事を取得して、エンジニア向けのブログとして公開しています。',
+    'HayaTech-Blogでは、エンジニアのための情報共有コミュニティであるZENNに投稿している記事を取得して、エンジニア向けのブログとして公開しています。',
   verification: { google: 'cNMPZcujlVSd4RRmrb1ARAxCMpTVxc7-DepsQRTKztY' },
   keywords:
-    'Next.js, React, TypeScript, JavaScript, Python, HTML, CSS, Web Development, Engineer, Blog, Idea, Tech',
-  authors: [{ name: 'Hayate', url: 'https://hayatech-blog.vercel.app/' }],
+    'Next.js, React, TypeScript, JavaScript, Python, PHP, SQL, HTML, CSS, Web Development, Engineer, Blog, Idea, Tech',
+  authors: [{ name: 'Hayate', url: 'https://hayatech-blog.vercel.app' }],
   icons: {
-    icon: './favicon.ico',
+    icon: [
+      { rel: 'icon', url: '/favicon.ico' },
+      { rel: 'apple-touch-icon', url: '/images/HayaTech.png' },
+      { rel: 'android-chrome', url: '/images/HayaTech.png' },
+    ],
+  },
+  metadataBase: new URL('https://hayatech-blog.vercel.app'),
+  alternates: {
+    canonical: '/',
   },
   openGraph: {
-    title: 'HayaTech-Blog',
+    title: hayaTechBlog,
     description:
-      'HayaTech-Blogでは、エンジニアのための情報共有コミュニティであるに投稿している記事を取得して、エンジニア向けのブログとして公開しています。',
+      'HayaTech-Blogでは、エンジニアのための情報共有コミュニティであるZENNに投稿している記事を取得して、エンジニア向けのブログとして公開しています。',
     url: 'https://hayatech-blog.vercel.app',
-    siteName: 'HayaTech-Blog',
+    siteName: hayaTechBlog,
     images: [
       {
         url: `https://og-image.vercel.app/${encodeURI(
@@ -37,52 +45,6 @@ export const metadata: Metadata = {
     title: hayaTechBlog,
   },
 };
-
-// export const metadata: Metadata = {
-//   title: hayaTechBlog,
-//   description:
-//     'HayaTech-Blogでは、エンジニアのための情報共有コミュニティであるZENNに投稿している記事を取得して、エンジニア向けのブログとして公開しています。',
-//   verification: { google: 'cNMPZcujlVSd4RRmrb1ARAxCMpTVxc7-DepsQRTKztY' },
-//   keywords:
-//     'Next.js, React, TypeScript, JavaScript, Python, PHP, SQL, HTML, CSS, Web Development, Engineer, Blog, Idea, Tech',
-//   authors: [{ name: 'Hayate', url: 'https://hayatech-blog.vercel.app' }],
-//   icons: {
-//     icon: '/favicon.ico',
-//     shortcut: '/images/HayaTech.png',
-//     apple: '/images/HayaTech.png',
-//     other: [
-//       {
-//         rel: 'android-chrome',
-//         url: '/images/HayaTech.png',
-//       },
-//     ],
-//   },
-//   metadataBase: new URL('https://hayatech-blog.vercel.app'),
-//   alternates: {
-//     canonical: '/',
-//   },
-//   openGraph: {
-//     title: hayaTechBlog,
-//     description:
-//       'HayaTech-Blogでは、エンジニアのための情報共有コミュニティであるZENNに投稿している記事を取得して、エンジニア向けのブログとして公開しています。',
-//     url: 'https://hayatech-blog.vercel.app',
-//     siteName: hayaTechBlog,
-//     images: [
-//       {
-//         url: 'https://hayatech-blog.vercel.app/images/HayaTech.png',
-//         width: 1200,
-//         height: 630,
-//         alt: hayaTechBlog,
-//       },
-//     ],
-//     type: 'website',
-//   },
-//   twitter: {
-//     card: 'summary_large_image',
-//     title: hayaTechBlog,
-//     images: ['https://hayatech-blog.vercel.app/images/HayaTech.png'],
-//   },
-// };
 
 /**************************************************
  * Layout
